fae9e1598d23fc8c454afd13f2b6431a5d282285
siddharthcmdz <siddharth.cmdz@gmail.com>
2022-11-09 20:28:18 -0800

    added support to load monkey model

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,46 +1,56 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+import Stats from 'three/examples/jsm/libs/stats.module'
 
 const scene = new THREE.Scene()
+scene.add(new THREE.AxesHelper(5))
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
 camera.position.z = 2
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-const controls = new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement);
+// controls.enableDamping = true;
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
+const loader = new GLTFLoader()
+loader.load(
+    'models/monkey.glb',
+    function (gltf) {
+            scene.add(gltf.scene)
+        },
+        (xhr) => {
+            console.log(xhr.loaded / xhr.total * 100 + '% loaded')
+        },
+        (error) => {error}
+);
 
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
 
-window.addEventListener('resize', onWindowResize, false)
+window.addEventListener('resize', onWindowResize, false);
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
+    camera.aspect = window.innerWidth/window.innerHeight;
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
     render()
 }
 
+const stats = Stats()
+document.body.appendChild(stats.dom)
+
 function animate() {
     requestAnimationFrame(animate)
-
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
-
     controls.update()
-
     render()
+
+    stats.update()
 }
 
 function render() {
     renderer.render(scene, camera)
 }
+
 animate()
+
