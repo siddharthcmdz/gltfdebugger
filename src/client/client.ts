7b4620e7101b1d7479fa9446d091235a66f381da
siddharthcmdz <siddharth.cmdz@gmail.com>
2022-11-09 20:35:39 -0800

    support draco compressor and physically correct lights

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
 import Stats from 'three/examples/jsm/libs/stats.module'
 
 const scene = new THREE.Scene()
@@ -10,13 +11,19 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHei
 camera.position.z = 2
 
 const renderer = new THREE.WebGLRenderer()
+renderer.physicallyCorrectLights = true
+renderer.shadowMap.enabled = true
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement);
 // controls.enableDamping = true;
 
-const loader = new GLTFLoader()
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('/js/libs/draco/');
+
+const loader = new GLTFLoader();
+loader.setDRACOLoader(dracoLoader);
 loader.load(
     'models/monkey.glb',
     function (gltf) {
